ba710c0cdf75bedd16f2bf52b87a691d5b9e4772
Nicoll Zarai Mantilla Miranda <160680324+NicoMantillaM@users.noreply.github.com>
2024-02-23 16:21:56 -0500

    solucion ejercicio area triangulo

---
+++ ejercicio6.py
@@ -0,0 +1,4 @@
+base=float(input("ingrese la base de el triangulo"))
+altura= float(float(input("ingrese la altura de el triangulo")))
+area= (base*altura)/2
+print("El area de el triangulo es:", area)
