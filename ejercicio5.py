3a11a78c99ce3cac08b2c85804cfe9c31b7cadd6
Nicoll Zarai Mantilla Miranda <160680324+NicoMantillaM@users.noreply.github.com>
2024-02-23 16:13:08 -0500

    solucion ejercicio par y impar

---
+++ ejercicio5.py
@@ -0,0 +1,5 @@
+numer= int(input("ingrese el numero que desea saber si es par o impar:" ))
+if numer % 2== 0:
+    print ("El numero ingresado es par")
+elif numer % 2 != 0:
+    print("El numero ingresado es impar") 
