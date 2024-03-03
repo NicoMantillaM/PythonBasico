a8cf150c72ff2b0604aab5e70e539f51db01feb6
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-02 22:20:56 -0500

    perimetro y area circulo

---
+++ circulos.py
@@ -0,0 +1,5 @@
+radio= input("ingrese el radio del circulo: ")
+perimetro= 2 * 3.14 * int(radio)
+area= 3.14 * int(radio) ** 2
+print ("El perimetro del circulo es:",perimetro )
+print ("El area del circulo es:",area )
