aa0e5b15aae12d92d5bc72397c505a729359eff8
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-02 22:42:56 -0500

    promedio de 4 notas

---
+++ promedio.py
@@ -0,0 +1,7 @@
+nota1= input("ingrese la Primera nota : ")
+nota2= input("ingrese la Segunda nota : ")
+nota3= input("ingrese la Tercera nota : ")
+nota4= input("ingrese la Cuarta nota : ")
+suma= int(nota1) + int(nota2) + int(nota3) + int(nota4)
+promedio= (suma)/4
+print (f"El promedio de sus notas es:", promedio)
