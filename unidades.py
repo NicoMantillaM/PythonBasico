59c08637d88a1c7123a95c88626d4875909d9a96
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-03 00:11:35 -0500

    unidades cm a pulgadas

---
+++ unidades.py
@@ -0,0 +1,3 @@
+centimetros= int(input("ingrese los centimetros que desea pasar a pulgadas: "))
+pulgadas= centimetros / 2.54 
+print (f"Los centimentros {centimetros}, en pulgadas son: {pulgadas}")
