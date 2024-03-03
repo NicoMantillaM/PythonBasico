63737acc18dd1968ec8d91612d90f52b7da7e7b1
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-03-03 18:11:18 -0500

    parte decimal

---
+++ parteDecimal.py
@@ -0,0 +1,5 @@
+numori=float(input("Ingrese un numero real: "))
+entero= int(numori) 
+decim= numori- entero
+decim= round (decim,2)
+print(f"La parte decimal del numero: {numori}, es igual a: {decim}")
